4cf26170be90ab9ce643f78624c947c5e78ef1a2
saikalbatyrbekova <132566790+saikalbatyrbekova@users.noreply.github.com>
2024-12-12 00:46:46 +0600

    Update CorsConfig.java

--- src/main/java/com/example/Memories_App/config/CorsConfig.java
+++ src/main/java/com/example/Memories_App/config/CorsConfig.java
@@ -1,22 +1,22 @@
-package com.example.Memories_App.config;
+// package com.example.Memories_App.config;
 
-import jakarta.annotation.Nullable;
-import jakarta.validation.constraints.NotNull;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import jakarta.annotation.Nullable;
+// import jakarta.validation.constraints.NotNull;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig {
+// @Configuration
+// public class CorsConfig {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
-            }
-        };
-    }
-}
+//     @Bean
+//     public WebMvcConfigurer corsConfigurer() {
+//         return new WebMvcConfigurerAdapter() {
+//             @Override
+//             public void addCorsMappings(CorsRegistry registry) {
+//                 registry.addMapping("/**");
+//             }
+//         };
+//     }
+// }
