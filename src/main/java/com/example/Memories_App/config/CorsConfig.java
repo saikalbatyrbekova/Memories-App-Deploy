44459ee20f13036fbcf456b2606b7a4e5cfae413
saikalbatyrbekova <saikal.batyrbekova@alatoo.edu.kg>
2024-12-11 19:41:09 +0600

    Cors Config adding

---
+++ src/main/java/com/example/Memories_App/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.example.Memories_App.config;
+
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NotNull @Nullable CorsRegistry registry) {
+                assert registry != null;
+                registry.addMapping("/**")
+                        .allowedOrigins("https://memories-app-deploy-production.up.railway.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
